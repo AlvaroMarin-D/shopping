7efe3d231ee10639d3a0a44a37d1e876aab4cda1
AlvaroMarin-D <alvaromarin.d@gmail.com>
2022-09-01 17:44:27 -0400

    Banner start

--- shopping/components/HeroBanner.jsx
+++ shopping/components/HeroBanner.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import Link from 'next/link'
+import { urlFor } from '../lib/client'
 
-const HeroBanner = () => {
+const HeroBanner = ({heroBanner}) => {
   return (
     <div className='hero-banner-container'>
       <div>
         <p className='beats-solo'>
-          Small Text Here
+          {heroBanner.smallText}
         </p>
         <h3>
-          Mid Text
+          {heroBanner.midText}
         </h3>
-        <img src="" alt="headphones" className="hero-banner-image" />
+        <h1>
+          {heroBanner.largeText1}
+        </h1>
+        <img src={urlFor(heroBanner.image)} alt="headphones" className="hero-banner-image" />
         <div>
           <Link href="/product/ID">
             <button type='button'>
